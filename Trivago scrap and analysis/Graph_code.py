f46d300e21f57e966a2d1078d24e16697791b2c9
DishaVaidh <76588194+DishaVaidh@users.noreply.github.com>
2021-08-07 20:42:04 +0530

    Add files via upload

--- Trivago scrap and analysis/Graph_code.py
+++ Trivago scrap and analysis/Graph_code.py
@@ -1,21 +1,32 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import webbrowser
+import plotly
+from flask import Flask, request, render_template
 
-#px.renderers.default = "browser"
-
-def plot_graph(c,st,maxPrice):
+def plot_graph(c,maxPrice,st=0):
+    app = Flask(__name__, template_folder='.')
     print(c)
-    df=pd.read_csv("C:\\trivago_data\\"+c+st+"star"+".csv")
-    #df=pd.read_csv(c+".csv")
-    # maximum=int(input("Enter maximum rate : "))
+    print(type(st))
+    df1=pd.read_csv("C:\\trivago_data\\"+c+st+"star"+".csv")
+    #df1 = pd.read_csv("jaipur.csv")
+    
     if(maxPrice != -1):
-        df=df.drop(df[df['Rate'] > maxPrice].index)
-    #print(df.describe())
-    g=df.groupby(by='checkin')
+        df1=df1.drop(df1[df1['Rate'] > maxPrice].index)
+    #print(df1.describe())
+    g=df1.groupby(by='checkin')
     a=g['Rate'].agg(np.mean)
     #print(a)
     b=a.to_frame(name="average_rates").reset_index()
     #b = px.data.gapminder()
     fig = px.line(b,x="checkin", y="average_rates", title='Rates in '+st+' stars hotels of '+c)
-    fig.show(renderer="browser")
+
+    htm = plotly.io.to_html(fig=fig, include_plotlyjs='cdn')
+    f  = open('reder.html','w')
+    f.write(htm)
+    @app.route('/')
+    def rungraph():
+        return render_template('reder.html') 
+    webbrowser.open_new("http://127.0.0.1:5000/")
+    app.run(host='127.0.0.1',port=5000)
